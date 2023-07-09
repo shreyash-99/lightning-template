772bba59cf8ddda073eafa42425731c1b4876a9a
Shreyash Gupta <109542611+shreyash-99@users.noreply.github.com>
2023-07-09 23:49:01 +0000

    added files for loggers

--- titanium/train.py
+++ titanium/train.py
@@ -1,4 +1,5 @@
 from typing import Tuple, Dict
+import mlflow
 
 import lightning as L
 import torch
@@ -22,15 +23,33 @@ def train(cfg: DictConfig) -> Tuple[dict, dict]:
     log.info(f"Instantiating model <{cfg.model._target_}>")
     model: LightningModule = hydra.utils.instantiate(cfg.model)
 
+    log.info("Instantiating loggers...")
+    logger: List[Logger] = utils.instantiate_loggers(cfg.get("logger"))
+
+
+
+    log.info("Instantiating callbacks...")
+    callbacks: List[Callback] = utils.instantiate_callbacks(cfg.get("callbacks"))
+
     log.info(f"Instantiating trainer <{cfg.trainer._target_}>")
-    trainer: Trainer = hydra.utils.instantiate(cfg.trainer)
+    trainer: L.Trainer = hydra.utils.instantiate(cfg.trainer, callbacks=callbacks, logger=logger)
 
     object_dict = {
         "cfg": cfg,
         "datamodule": datamodule,
         "model": model,
         "trainer": trainer,
+        "callbacks": callbacks,
+        "logger": logger,
     }
+
+    if logger:
+        log.info("Logging hyperparameters!")
+        utils.log_hyperparameters(object_dict)
+
+    if cfg.get("compile"):
+        model = torch.compile(model)
+
 
     if cfg.get("train"):
         log.info("Starting training!")
@@ -41,13 +60,25 @@ def train(cfg: DictConfig) -> Tuple[dict, dict]:
 
     if cfg.get("test"):
         log.info("Starting testing!")
-        ckpt_path = ""
+        ckpt_path = trainer.checkpouint_callback.best_model_path
         if ckpt_path == "":
             log.warning(
                 "Best ckpt not found! Using current weights for testing...")
             ckpt_path = None
         trainer.test(model=model, datamodule=datamodule, ckpt_path=ckpt_path)
         log.info(f"Best ckpt path: {ckpt_path}")
+
+        for logger_ in logger:
+            if isinstance(logger_, L.pytorch.loggers.mlflow.MLFlowLogger)):
+                ckpt = torch.load(ckpt_path)
+                model.load_state_dict(ckpt["state_dict"])
+                os.environ['MLFLOW_RUN_ID'] = logger_.run_id
+                os.environ['MLFLOW_EXPERIMENT_ID'] = logger_.experiment_id
+                os.environ['MLFLOW_EXPERIMENT_NAME'] = logger_._experiment_name
+                os.environ['MLFLOW_TRACKING_URI'] = logger_._tracking_uri
+                mlflow.pytorch.log_model(model, "model")
+                break
+
 
     test_metrics = trainer.callback_metrics
 
--- titanium/utils/__init__.py
+++ titanium/utils/__init__.py
@@ -1,2 +1,4 @@
 from titanium.utils.pylogger import get_pylogger
 from titanium.utils.utils import task_wrapper, get_metric_value
+from titanium.utils.instantiators import instantiate_callbacks, instantiate_loggers
+from titanium.utils.logging_utils import log_hyperparameters
